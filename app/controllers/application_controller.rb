1836b5fa5becd993e2a903b3b250adf4d5e837f1
github username <github email address>
2017-06-29 14:55:02 -0400

    CRUDgit add .git add .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts/new' do
+    erb :'/posts/new'
   end
+
+  post '/posts' do
+    @post = Post.create(params[:post])
+
+    redirect "/posts"
+  end
+
+  get '/posts' do
+    erb :index
+  end
+
+  patch '/posts/:id' do
+    Post.find_by_id(params[:id]).update(params[:post])
+    @post = Post.find_by_id(params[:id])
+    redirect "/posts/#{@post.id}"
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :'/posts/show'
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :'/posts/edit'
+  end
+
+  delete '/posts/:id/delete' do
+    @post_name = Post.find_by_id(params[:id]).name
+    Post.find_by_id(params[:id]).destroy
+    erb :'/posts/delete'
+
+  end
+
 end
